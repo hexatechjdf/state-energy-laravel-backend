f9676b032fa8729e77d5f952ffab18d6a337fbcf
Farhan Zafar <farhanzafar1643@gmail.com>
2025-10-27 18:37:42 +0500

    disposition api and lender api settting

--- app/Http/Controllers/Api/V1/DispositionController.php
+++ app/Http/Controllers/Api/V1/DispositionController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Api\V1\OrderResource;
+use App\Http\Resources\DispositionResource;
 use App\Models\Disposition;
 
 class DispositionController extends Controller
 {
     public function index()
     {
-        $orders = Disposition::all();
+        $disposition = Disposition::all();
         return successResponse([
-            'dispositions'  => OrderResource::collection($orders),
+            'dispositions'  => DispositionResource::collection($disposition),
         ]);
     }
 
--- routes/api/v1/lender.php
+++ routes/api/v1/lender.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Api\V1\LenderController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('disposition', LenderController::class);
+Route::apiResource('lender', LenderController::class);
